0158739d124c32eaec5f9c6b5e33b979cebf3c54
admalledd <admalledd@gmail.com>
2012-06-25 15:55:30 -0700

    initial working commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<TITLE>admalledd's minecraft server</
+<link rel="stylesheet" type="text/css" href="main.css" />
+
+</head>
+
+<body>
+
+<p> Online players:<pre class="onlinelist">
+<?php
+$ops =    file_get_contents("/home/admalledd/minecraft/cur/ops.txt","r");
+$players =file_get_contents("/home/admalledd/minecraft/cur/ops.txt","r");
+
+
+
+
+?>
+</pre>
+</p>
+<h4>Plugins</h4>
+<a href="">This is a link to nowhere</a>
+</body>
+</html>
