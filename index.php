65d0f29bcc0f66187f089594cdd8d57a63b60474
Eric Driggers <admalledd@gmail.com>
2012-06-25 19:42:24 -0700

    skeleton player and moderator list complete, blank css added

--- index.php
+++ index.php
@@ -7,30 +7,37 @@
 
 <body>
 
-<p> Online players:<pre class="onlinelist">
+<p> Moderators/Admins:
+<pre class="modlist">
 <?php
+//first, get mods/admins
 $ops =    file_get_contents("/home/admalledd/minecraft/cur/ops.txt","r");
 echo $ops;
+
+//now we get players, using the rcon package
 require __DIR__ . '/rcon' . '/MinecraftQuery.class.php';
 
 $Query = new MinecraftQuery( );
 try
 {
-    $Query->Connect( 'localhost', 27015, 1 );
-    print_r( $Query->GetInfo( ) );
-    print_r( $Query->GetPlayers( ) );
+    $Query->Connect( 'localhost', 25565, 1 );
+    //if we can connect to the server: get the playerlist
+    $players = $Query->GetPlayers( );
 }
 catch( MinecraftQueryException $e )
 {
     echo $e->getMessage( );
+    //seems we failed to connect to the mc server, echo the error and a human explination
+    $players = "could not connect to server for data, try again later";
 }
 
-//$players =file_get_contents("/home/admalledd/minecraft/cur/ops.txt","r");
-
-
-
-
 ?>
+
+
+</pre>
+</p>
+<p> Online players:<pre class="onlinelist">
+<?php print_r( $players );/*print players here and now, I know i could have done it sooner, but meh*/ ?>
 </pre>
 </p>
 <h4>Plugins</h4>
